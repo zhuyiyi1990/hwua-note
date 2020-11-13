ad64d839914c01647f17114024e15e0b22a9b436
朱一一 <zhuyiyi1990@outlook.com>
2020-11-13 17:25:41 +0800

    20201113 Mybatis笔记1.md 从添加测试通过id查找用户的方法开始看

--- src/test/java/com/github/zhuyiyi1990/test/UserTest.java
+++ src/test/java/com/github/zhuyiyi1990/test/UserTest.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -13,22 +15,43 @@ import java.io.InputStream;
 import java.util.List;
 
 public class UserTest {
-    @Test
-    public void testFindAll() throws IOException {
-        InputStream in = Resources.getResourceAsStream("SqlMapperConfig.xml");
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
-        SqlSession sqlSession = factory.openSession(true);
-        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
-        List<User> list = userDao.findAll();
-        for (User user : list) {
-            System.out.println(user);
-        }
+    private InputStream in;
+
+    private SqlSessionFactory factory;
+
+    private SqlSession sqlSession;
+
+    private IUserDao userDao;
+
+    @Before
+    public void init() throws IOException {
+        in = Resources.getResourceAsStream("SqlMapperConfig.xml");
+        factory = new SqlSessionFactoryBuilder().build(in);
+        sqlSession = factory.openSession(true);
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
+
+    @After
+    public void close() throws IOException {
         if (sqlSession != null) {
             sqlSession.close();
         }
         if (in != null) {
             in.close();
         }
+    }
+
+    @Test
+    public void testFindAll() throws IOException {
+        List<User> list = userDao.findAll();
+        for (User user : list) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testFindById() {
+        User user = userDao.findById(3);
+        System.out.println(user);
     }
 }
