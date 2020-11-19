efa9593de2eed62a3e09813ec9080f2731218d77
朱一一 <zhuyiyi1990@outlook.com>
2020-11-19 16:01:54 +0800

    20201119 Spring5笔记day02.md

---
+++ src/main/java/com/github/zhuyiyi1990/config/SpringConfiguration.java
@@ -0,0 +1,11 @@
+package com.github.zhuyiyi1990.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan({"com.github.zhuyiyi1990"})
+@Import(JdbcConfig.class)
+public class SpringConfiguration {
+}
