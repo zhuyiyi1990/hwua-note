1e5c1ff0d321dcf631895da1a5e589ccdfd194b7
朱一一 <zhuyiyi1990@outlook.com>
2020-11-20 17:13:30 +0800

    20201120 Spring5笔记day03.md

--- src/main/java/com/github/zhuyiyi1990/service/IAccountService.java
+++ src/main/java/com/github/zhuyiyi1990/service/IAccountService.java
@@ -1,17 +1,9 @@
 package com.github.zhuyiyi1990.service;
 
-import com.github.zhuyiyi1990.pojo.Account;
-
-import java.util.List;
-
 public interface IAccountService {
-    public abstract List<Account> findAll();
+    public abstract void saveAccount();
 
-    public abstract Account findById(int id);
+    public abstract void updateAccount(int i);
 
-    public abstract int save(Account account);
-
-    public abstract int update(Account account);
-
-    public abstract int delete(int id);
+    public abstract int deleteAccount();
 }
---
+++ src/test/java/com/github/zhuyiyi1990/test/MyTest.java
@@ -0,0 +1,21 @@
+package com.github.zhuyiyi1990.test;
+
+import com.github.zhuyiyi1990.config.SpringConfig;
+import com.github.zhuyiyi1990.service.IAccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SpringConfig.class})
+public class MyTest {
+    @Autowired
+    private IAccountService accountService;
+
+    @Test
+    public void test() {
+        accountService.saveAccount();
+    }
+}
