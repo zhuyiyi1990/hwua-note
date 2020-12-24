3cbe61e6f59175a16df2bde2dbd7bf8bac1b903e
朱一一 <zhuyiyi1990@outlook.com>
2020-12-24 15:40:31 +0800

    20201224 Spring5笔记day02.md

--- src/main/java/com/github/zhuyiyi1990/dao/impl/AccountDaoImpl.java
+++ src/main/java/com/github/zhuyiyi1990/dao/impl/AccountDaoImpl.java
@@ -1,10 +1,67 @@
 package com.github.zhuyiyi1990.dao.impl;
 
 import com.github.zhuyiyi1990.dao.IAccountDao;
+import com.github.zhuyiyi1990.pojo.Account;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository("accountDao")
 public class AccountDaoImpl implements IAccountDao {
+    @Autowired
+    private QueryRunner runner;
+
+    public void setRunner(QueryRunner runner) {
+        this.runner = runner;
+    }
+
     @Override
-    public void saveAccount() {
-        System.out.println("保存用户成功");//简单模拟即可
+    public List<Account> findAll() {
+        try {
+            return runner.query("select * from account2", new BeanListHandler<Account>(Account.class));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Account findById(int id) {
+        try {
+            return runner.query("select * from account2 where id = ?", new BeanHandler<Account>(Account.class), id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int save(Account account) {
+        try {
+            return runner.update("insert into account2(name,money) values(?,?)", account.getName(), account.getMoney());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int update(Account account) {
+        try {
+            return runner.update("update account2 set name = ?, money = ? where id = ?", account.getName(), account.getMoney(), account.getId());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int delete(int id) {
+        try {
+            return runner.update("delete from account2 where id = ?", id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
