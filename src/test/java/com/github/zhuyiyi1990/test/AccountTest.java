a96e3a1685e8c0734a0c634d26c644df694ed671
朱一一 <zhuyiyi1990@outlook.com>
2021-01-26 11:15:03 +0800

    20210126 Mybatis笔记2.md

---
+++ src/test/java/com/github/zhuyiyi1990/test/AccountTest.java
@@ -0,0 +1,48 @@
+package com.github.zhuyiyi1990.test;
+
+import com.github.zhuyiyi1990.dao.IAccountDao;
+import com.github.zhuyiyi1990.pojo.Account;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class AccountTest {
+    private InputStream in;
+
+    private SqlSessionFactory factory;
+
+    private SqlSession sqlSession;
+
+    private IAccountDao accountDao;
+
+    @Before
+    public void init() throws IOException {
+        in = Resources.getResourceAsStream("SqlMapperConfig.xml");
+        factory = new SqlSessionFactoryBuilder().build(in);
+        sqlSession = factory.openSession(true);
+        accountDao = sqlSession.getMapper(IAccountDao.class);
+    }
+
+    @After
+    public void close() throws IOException {
+        if (sqlSession != null) {
+            sqlSession.close();
+        }
+        if (in != null) {
+            in.close();
+        }
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Account> list = accountDao.findAll();
+    }
+}
