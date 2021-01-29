8ce68c4448f0148637290702143e7e2611a8253f
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-01-29 15:38:42 +0800

    20210129 Spring5笔记day01.md

---
+++ src/main/java/com/github/zhuyiyi1990/factory/BeanFactory.java
@@ -0,0 +1,35 @@
+package com.github.zhuyiyi1990.factory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class BeanFactory {
+
+    private static Properties props;
+
+    private static Map<String, Object> beanMap;
+
+    static {
+        try {
+            props = new Properties();
+            InputStream in = BeanFactory.class.getClassLoader().getResourceAsStream("bean.properties");
+            props.load(in);
+            beanMap = new HashMap<>();
+            Enumeration<Object> keys = props.keys();
+            while (keys.hasMoreElements()) {
+                String key = keys.nextElement().toString();
+                String beanPath = props.getProperty(key);
+                beanMap.put(key, Class.forName(beanPath).newInstance());
+            }
+        } catch (Exception e) {
+            //抛Error,程序会中断
+            throw new ExceptionInInitializerError("初始化properties失败!");
+        }
+    }
+
+    public static Object getBean(String beanName) {
+        return beanMap.get(beanName);
+    }
+
+}
---
+++ src/main/java/com/github/zhuyiyi1990/service/impl/AccountServiceImpl2.java
@@ -0,0 +1,44 @@
+package com.github.zhuyiyi1990.service.impl;
+
+import com.github.zhuyiyi1990.service.IAccountService;
+
+import java.util.Date;
+
+public class AccountServiceImpl2 implements IAccountService {
+
+    private String name;
+
+    private Integer age;
+
+    private Date birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public void saveAccount() {
+        System.out.println("AccountServiceImpl中的saveAccount执行了");
+    }
+
+}
