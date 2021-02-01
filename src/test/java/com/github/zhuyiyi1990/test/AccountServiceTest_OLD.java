fb6c945c89ac4263f3974d64f112634e28b3ec0b
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-02-01 13:55:32 +0800

    20210201 Spring5笔记day02.md

---
+++ src/test/java/com/github/zhuyiyi1990/test/AccountServiceTest_OLD.java
@@ -0,0 +1,60 @@
+package com.github.zhuyiyi1990.test;
+
+import com.github.zhuyiyi1990.pojo.Account;
+import com.github.zhuyiyi1990.service.IAccountService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class AccountServiceTest_OLD {
+
+    private ApplicationContext ac;
+
+    private IAccountService accountService;
+
+    @Before
+    public void init() {
+        ac = new ClassPathXmlApplicationContext("bean.xml");
+        accountService = ac.getBean("accountService", IAccountService.class);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Account> list = accountService.findAll();
+        for (Account account : list) {
+            System.out.println(account);
+        }
+    }
+
+    @Test
+    public void testFindOne() {
+        Account account = accountService.findById(1);
+        System.out.println(account);
+    }
+
+    @Test
+    public void testSave() {
+        Account account = new Account();
+        account.setName("小明");
+        account.setMoney(200);
+        accountService.save(account);
+    }
+
+    @Test
+    public void testUpdate() {
+        Account account = new Account();
+        account.setId(114);
+        account.setName("xiaoming");
+        account.setMoney(2000);
+        accountService.update(account);
+    }
+
+    @Test
+    public void testDelete() {
+        accountService.delete(114);
+    }
+
+}
