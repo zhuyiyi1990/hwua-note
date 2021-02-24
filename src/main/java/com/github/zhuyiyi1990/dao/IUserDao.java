5a6a4fecb56eaea37dfa90e3574e3027a65d9495
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-02-24 18:01:09 +0800

    20210224 Mybatis笔记1.md 8.编写测试类

---
+++ src/main/java/com/github/zhuyiyi1990/dao/IUserDao.java
@@ -0,0 +1,16 @@
+package com.github.zhuyiyi1990.dao;
+
+import com.github.zhuyiyi1990.pojo.User;
+
+import java.util.List;
+
+public interface IUserDao {
+
+    /**
+     * 查找所有的用户
+     *
+     * @return
+     */
+    public abstract List<User> findAll();
+
+}
---
+++ src/test/java/com/github/zhuyiyi1990/test/UserTest.java
@@ -0,0 +1,36 @@
+package com.github.zhuyiyi1990.test;
+
+import com.github.zhuyiyi1990.dao.IUserDao;
+import com.github.zhuyiyi1990.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class UserTest {
+
+    @Test
+    public void testFindAll() throws IOException {
+        InputStream in = Resources.getResourceAsStream("SqlMapperConfig.xml");
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(in);
+        SqlSession sqlSession = factory.openSession(true);
+        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
+        List<User> list = userDao.findAll();
+        for (User user : list) {
+            System.out.println(user);
+        }
+        if (sqlSession != null) {
+            sqlSession.close();
+        }
+        if (in != null) {
+            in.close();
+        }
+    }
+
+}
