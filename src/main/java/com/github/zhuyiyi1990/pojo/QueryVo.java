3e3e2bbd3957dfbd134fa6584cea4686401ee9eb
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-02-25 14:08:08 +0800

    20210225 Mybatis笔记1.md

---
+++ src/main/java/com/github/zhuyiyi1990/pojo/QueryVo.java
@@ -0,0 +1,17 @@
+package com.github.zhuyiyi1990.pojo;
+
+import java.io.Serializable;
+
+public class QueryVo implements Serializable {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
