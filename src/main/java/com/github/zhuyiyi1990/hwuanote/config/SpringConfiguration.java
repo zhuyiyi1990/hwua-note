2e83bdd2e137ca593ac79963efd3e3eabd7364eb
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-06-03 16:25:33 +0800

    20210603 Spring5笔记day03.md

--- src/main/java/com/github/zhuyiyi1990/hwuanote/config/SpringConfiguration.java
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/config/SpringConfiguration.java
@@ -2,10 +2,10 @@ package com.github.zhuyiyi1990.hwuanote.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@ComponentScan("com.github.zhuyiyi1990.hwuanote")
-@Import(JdbcConfig.class)
+@ComponentScan({"com.github.zhuyiyi1990.hwuanote"})
+@EnableAspectJAutoProxy
 public class SpringConfiguration {
 }
--- src/main/java/com/github/zhuyiyi1990/hwuanote/service/impl/AccountServiceImpl.java
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/service/impl/AccountServiceImpl.java
@@ -1,64 +1,25 @@
 package com.github.zhuyiyi1990.hwuanote.service.impl;
 
-import com.github.zhuyiyi1990.hwuanote.dao.IAccountDao;
-import com.github.zhuyiyi1990.hwuanote.pojo.Account;
 import com.github.zhuyiyi1990.hwuanote.service.IAccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.util.List;
-
-@Service("accountService")
+@Service
 public class AccountServiceImpl implements IAccountService {
 
-    @Autowired
-    @Qualifier("accountDao")
-    private IAccountDao accountDao;
-
-    public IAccountDao getAccountDao() {
-        return accountDao;
-    }
-
-    public void setAccountDao(IAccountDao accountDao) {
-        this.accountDao = accountDao;
+    @Override
+    public void saveAccount() {
+        System.out.println("保存账户");
     }
 
     @Override
-    public List<Account> findAll() {
-        return accountDao.findAll();
+    public void updateAccount(int i) {
+        System.out.println("更新账户");
     }
 
     @Override
-    public Account findById(int id) {
-        return accountDao.findById(id);
-    }
-
-    @Override
-    public int save(Account account) {
-        return accountDao.save(account);
-    }
-
-    @Override
-    public int update(Account account) {
-        return accountDao.update(account);
-    }
-
-    @Override
-    public int delete(int id) {
-        return getAccountDao().delete(id);
-    }
-
-    @PostConstruct
-    public void init() {
-        System.out.println("初始化方法执行");
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println("销毁方法执行");
+    public int deleteAccount() {
+        System.out.println("删除账户");
+        return 0;
     }
 
 }
