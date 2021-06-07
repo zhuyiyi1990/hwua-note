f692eabfac894554816b96586a996c5e318a41ae
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-06-07 16:43:18 +0800

    20210607 Spring5笔记day04.md

--- src/main/java/com/github/zhuyiyi1990/hwuanote/service/impl/AccountServiceImpl.java
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/service/impl/AccountServiceImpl.java
@@ -1,25 +1,39 @@
 package com.github.zhuyiyi1990.hwuanote.service.impl;
 
+import com.github.zhuyiyi1990.hwuanote.dao.IAccountDao;
+import com.github.zhuyiyi1990.hwuanote.pojo.Account;
 import com.github.zhuyiyi1990.hwuanote.service.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 public class AccountServiceImpl implements IAccountService {
 
-    @Override
-    public void saveAccount() {
-        System.out.println("保存账户");
+    @Autowired
+    private IAccountDao accountDao;
+
+    public void setAccountDao(IAccountDao accountDao) {
+        this.accountDao = accountDao;
     }
 
     @Override
-    public void updateAccount(int i) {
-        System.out.println("更新账户");
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+    public Account findAccountById(int id) {
+        return accountDao.findAccountById(id);
     }
 
     @Override
-    public int deleteAccount() {
-        System.out.println("删除账户");
-        return 0;
+    public void transfer(String sourceName, String targetName, float money) {
+        Account sourceAccount = accountDao.findAccountByName(sourceName);
+        Account targetAccount = accountDao.findAccountByName(targetName);
+        sourceAccount.setMoney(sourceAccount.getMoney() - money);
+        targetAccount.setMoney(targetAccount.getMoney() + money);
+        accountDao.update(sourceAccount);
+//        int i = 1 / 0;
+        accountDao.update(targetAccount);
     }
 
 }
