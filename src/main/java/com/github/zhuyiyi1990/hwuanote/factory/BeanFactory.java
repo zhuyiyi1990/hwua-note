92a5a3ee266f7a1a73d1d0fba0cc637b0afc3d9d
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-06-18 14:16:15 +0800

    20210618 Spring5笔记day01.md

---
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/factory/BeanFactory.java
@@ -0,0 +1,35 @@
+package com.github.zhuyiyi1990.hwuanote.factory;
+
+import java.io.InputStream;
+import java.util.*;
+
+public class BeanFactory {
+
+    private static Properties props;
+
+    private static Map<String, Object> beanMap;
+
+    static {
+        try {
+            props = new Properties();
+            InputStream in = BeanFactory.class.getClassLoader().getResourceAsStream("bean.properties");
+            props.load(in);
+            beanMap = new HashMap<>();
+            List<String> keys = new ArrayList<>();
+            keys.add("accountDao");
+            keys.add("accountService");
+            for (String key : keys) {
+                String beanPath = props.getProperty(key);
+                beanMap.put(key, Class.forName(beanPath).newInstance());
+            }
+        } catch (Exception e) {
+            //抛Error,程序会中断
+            throw new ExceptionInInitializerError("初始化properties失败!");
+        }
+    }
+
+    public static Object getBean(String beanName) {
+        return beanMap.get(beanName);
+    }
+
+}
