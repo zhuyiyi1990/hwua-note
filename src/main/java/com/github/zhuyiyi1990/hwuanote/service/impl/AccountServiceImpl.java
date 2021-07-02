232f825f8ca5c1c33a94e43aa4639e6ab50beea5
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-07-02 11:23:03 +0800

    20210702 SpringMVC03.md

---
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/service/impl/AccountServiceImpl.java
@@ -0,0 +1,29 @@
+package com.github.zhuyiyi1990.hwuanote.service.impl;
+
+import com.github.zhuyiyi1990.hwuanote.dao.IAccountDao;
+import com.github.zhuyiyi1990.hwuanote.pojo.Account;
+import com.github.zhuyiyi1990.hwuanote.service.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("accountService")
+public class AccountServiceImpl implements IAccountService {
+
+    @Autowired
+    private IAccountDao accountDao;
+
+    @Override
+    public List<Account> findAll() {
+        System.out.println("业务层:查询所有账户...");
+        return accountDao.findAll();
+    }
+
+    @Override
+    public void saveAccount(Account account) {
+        System.out.println("业务层:保存账户...");
+        accountDao.saveAccount(account);
+    }
+
+}
