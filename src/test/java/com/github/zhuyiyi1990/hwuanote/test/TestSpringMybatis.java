c5f646447116a2eed1fe19b685d0405f1c0e4945
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-08-02 11:13:09 +0800

    20210802 SpringMVC03.md

---
+++ src/test/java/com/github/zhuyiyi1990/hwuanote/test/TestSpringMybatis.java
@@ -0,0 +1,36 @@
+package com.github.zhuyiyi1990.hwuanote.test;
+
+import com.github.zhuyiyi1990.hwuanote.pojo.Account;
+import com.github.zhuyiyi1990.hwuanote.service.IAccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:bean.xml")
+public class TestSpringMybatis {
+
+    @Autowired
+    private IAccountService accountService;
+
+    @Test
+    public void testFindAll() {
+        List<Account> accounts = accountService.findAll();
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+    }
+
+    @Test
+    public void testSaveAccount() {
+        Account account = new Account();
+        account.setName("tt");
+        account.setMoney(300F);
+        accountService.saveAccount(account);
+    }
+
+}
