07f26b60f0437cc18ef3e0c301c6ab067d0b81d5
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-08-06 11:22:11 +0800

    20210806 Spring5笔记day02.md

---
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/config/SpringConfiguration.java
@@ -0,0 +1,11 @@
+package com.github.zhuyiyi1990.hwuanote.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan({"com.github.zhuyiyi1990.hwuanote"})
+@Import({JdbcConfig.class})
+public class SpringConfiguration {
+}
