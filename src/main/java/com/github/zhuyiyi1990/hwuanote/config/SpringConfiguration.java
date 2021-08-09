418070ed75e5f0e8433f17366b45f6c001c473fd
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-08-09 14:17:50 +0800

    20210809 Spring5笔记day03.md

--- src/main/java/com/github/zhuyiyi1990/hwuanote/config/SpringConfiguration.java
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/config/SpringConfiguration.java
@@ -2,10 +2,10 @@ package com.github.zhuyiyi1990.hwuanote.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan({"com.github.zhuyiyi1990.hwuanote"})
-@Import({JdbcConfig.class})
+@EnableAspectJAutoProxy
 public class SpringConfiguration {
 }
--- src/main/java/com/github/zhuyiyi1990/hwuanote/service/IAccountService.java
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/service/IAccountService.java
@@ -1,19 +1,26 @@
 package com.github.zhuyiyi1990.hwuanote.service;
 
-import com.github.zhuyiyi1990.hwuanote.pojo.Account;
-
-import java.util.List;
-
 public interface IAccountService {
 
-    abstract public List<Account> findAll();
+    //以下三个方法为了选择三类：无参无返回值，有参无返回值，无参有返回值，有参有返回值相当于前两个方法组合
 
-    abstract public Account findById(int id);
+    /**
+     * 模拟保存账户
+     */
+    abstract public void saveAccount();
 
-    abstract public int save(Account account);
+    /**
+     * 模拟更新账户
+     *
+     * @param i
+     */
+    abstract public void updateAccount(int i);
 
-    abstract public int update(Account account);
-
-    abstract public int delete(int id);
+    /**
+     * 删除账户
+     *
+     * @return
+     */
+    abstract public int deleteAccount();
 
 }
