3c25c56fbff4d9ed464b89196cbcd2042ff297b1
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-08-19 15:53:09 +0800

    20210819 Spring5笔记day01.md

--- src/main/java/com/github/zhuyiyi1990/hwuanote/dao/IAccountDao.java
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/dao/IAccountDao.java
@@ -1,24 +1,7 @@
 package com.github.zhuyiyi1990.hwuanote.dao;
 
-import com.github.zhuyiyi1990.hwuanote.pojo.Account;
-
-import java.util.List;
-
 public interface IAccountDao {
 
-    /**
-     * 查询所有的账户,同时包含用户信息
-     *
-     * @return
-     */
-    abstract public List<Account> findAll();
-
-    /**
-     * 通过用户id查找对应的账户
-     *
-     * @param uid
-     * @return
-     */
-    abstract public List<Account> findByUid(Integer uid);
+    abstract public void saveAccount();
 
 }
