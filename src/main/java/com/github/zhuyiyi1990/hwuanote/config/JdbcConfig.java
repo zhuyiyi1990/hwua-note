54b3cd5baf87891f155aeb0c972beb55b5a42c05
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-08-25 15:07:39 +0800

    20210825 Spring5笔记day04.md

---
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/config/JdbcConfig.java
@@ -0,0 +1,42 @@
+package com.github.zhuyiyi1990.hwuanote.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@PropertySource({"classpath:jdbcConfig.properties"})
+public class JdbcConfig {
+
+    @Value("${jdbc.driver}")
+    private String driver;
+
+    @Value("${jdbc.url}")
+    private String url;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean("dataSource")
+    public DataSource createDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean("jdbcTemplate")
+    public JdbcTemplate createJdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+}
