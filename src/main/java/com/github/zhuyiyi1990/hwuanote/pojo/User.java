ad2e6df68715a1ddf7fcc83a357c5ddf185bccf2
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-08-26 14:29:11 +0800

    20210826 SpringMVC01.md

---
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/pojo/User.java
@@ -0,0 +1,46 @@
+package com.github.zhuyiyi1990.hwuanote.pojo;
+
+import java.util.Date;
+
+public class User {
+
+    private String uname;
+
+    private int age;
+
+    private Date birthday;//添加生日属性,java.util.Date类型
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uname='" + uname + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+}
