3efe21aa5bc10b28a3f1f36acc1d155afba21218
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-09-07 11:22:52 +0800

    20210907 Spring5笔记day02.md

--- src/main/java/com/github/zhuyiyi1990/hwuanote/service/IAccountService.java
+++ src/main/java/com/github/zhuyiyi1990/hwuanote/service/IAccountService.java
@@ -1,7 +1,19 @@
 package com.github.zhuyiyi1990.hwuanote.service;
 
+import com.github.zhuyiyi1990.hwuanote.pojo.Account;
+
+import java.util.List;
+
 public interface IAccountService {
 
-    abstract public void saveAccount();
+    abstract public List<Account> findAll();
+
+    abstract public Account findById(int id);
+
+    abstract public int save(Account account);
+
+    abstract public int update(Account account);
+
+    abstract public int delete(int id);
 
 }
